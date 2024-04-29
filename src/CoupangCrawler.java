400ea27d4c886e10ff61277b601fb303327b7ec5
gguip1 <gguip7554@naver.com>
2024-04-30 02:16:21 +0900

    CrawlerBase
    크롤링 하기 편하도록 CrawlerBase 클래스 생성
    **주석 참고**

---
+++ src/CoupangCrawler.java
@@ -0,0 +1,50 @@
+import crawler.CrawlerBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CoupangCrawler extends CrawlerBase{
+
+    public CoupangCrawler(String URL) {
+        super(URL);
+    }
+    private WebDriver webDriver = super.getWebDriver();
+    private WebElement element;
+
+    public void activate(){
+        try{
+            webDriver.get(super.getURL());
+            Thread.sleep(2000);
+
+            /**
+             * html의 xpath를 이용해서 크롤링
+             * 플랫폼 별 xpath 위치를 설정해서 자동화할 필요가 있음
+             */
+            element = webDriver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[3]/div[1]/div[2]/div/span[1]"));
+            String description = element.getText();
+            element = webDriver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[3]/div[2]/div[2]/div[1]/span[2]"));
+            String genre = element.getText();
+            element = webDriver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[3]/div[2]/div[2]/div[2]/span[2]"));
+            String actor = element.getText();
+            element = webDriver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[3]/div[2]/div[2]/div[3]/span[2]"));
+            String director = element.getText();
+            element = webDriver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div[3]/div[2]/div[2]/div[4]/span[2]"));
+            String writer = element.getText();
+
+            /**
+             * 크롤링 출력
+             */
+            System.out.println(description);
+            System.out.println(genre);
+            System.out.println(actor);
+            System.out.println(director);
+            System.out.println(writer);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally{
+            webDriver.close();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        CoupangScrapEX coupangsCrap = new CoupangScrapEX();
-        coupangsCrap.activate();
+
+        String URL = "https://www.coupangplay.com/content/1c6b40a9-2c02-4d34-af8e-ec3af17118a3";
+        CoupangCrawler coupangCrawler = new CoupangCrawler(URL);
+        coupangCrawler.activate();
+
     }
 }
